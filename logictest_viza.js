a61433f92e61a554d487c7a7ce0717b5af832982
viza <avizaanandya12@gmail.com>
2022-06-19 23:27:27 +0700

    reversewords function added

--- logictest_viza.js
+++ logictest_viza.js
@@ -9,3 +9,20 @@ function maxRedigit(num){
   }
   
 maxRedigit(123);
+
+// Map Array Data
+
+// Alternate Case
+
+// Multiple 3 and 5
+
+// Nearest Fibonacci
+
+// Reverse Words
+function reverseWords(text){
+  let reverseArr = text.split(" ").map(word => word.split("").reverse().join(""));
+  return reverseArr.join(" ");
+}
+
+reverseWords("i am a great human")
+// Find Middle Alphabet
